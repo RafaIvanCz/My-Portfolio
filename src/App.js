4edd97687e4dcfcb1c4d11e4036bf572879db1b5
RafaelivanCz <rafaelivancz@gmail.com>
2022-11-14 00:28:48 -0300

    I created the particles background

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Hero from "./Components/Hero/Hero";
 import LateralProfile from "./Components/LateralProfile/LateralProfile";
+import Particle from "./Components/Particles/Particles";
 import Portfolio from "./Components/Portfolio/Portfolio";
 
-
 const App = () => {
+
   return (
     <section>
       <LateralProfile />
+      <Particle />
       <div>
         <Hero />
         <About />
---
+++ src/Components/Particles/Particles.js
@@ -0,0 +1,100 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Particle = () => {
+
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    await loadFull(engine);
+}, []);
+
+const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+}, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#e7d8c9",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "grab",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            grab: {
+              distance: 140,
+              duration: 0.5,
+              line_linked: {
+                "opacity": 1
+              }
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#012a4a",
+          },
+          links: {
+            color: "#012a4a",
+            distance: 200,
+            enable: true,
+            opacity: 0.2,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1.5,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 130,
+          },
+          opacity: {
+            value: 0.3,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default Particle;
